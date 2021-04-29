a45115419c9832b84bd64418b1c8a1a99b0ccfc1
ahmednnasser11@gmail.com <ahmednnasser11@gmail.com>
2021-04-29 21:53:15 +0200

    start the project

---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAlHz19-JvkTU7m9b1FGPks_NFQhDYTzVo",
+    authDomain: "e-clone-yt.firebaseapp.com",
+    projectId: "e-clone-yt",
+    storageBucket: "e-clone-yt.appspot.com",
+    messagingSenderId: "843219817221",
+    appId: "1:843219817221:web:933b211481f07f0d2b5d6c",
+    measurementId: "G-L59NQV2WVQ"
+};
